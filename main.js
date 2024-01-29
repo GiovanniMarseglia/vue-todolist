23f393ffde63de8a2f68a140a7964806a8f696ea
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-01-29 15:18:19 +0100

    Completed

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+          todo:[
+            
+          ],
+         testo:""
+      }
+    },
+      methods: {
+        
+
+        remove(index){
+          setTimeout(()=>{
+            this.todo.splice(index,1)
+          }, 1)
+          
+        },
+        add(){
+          if(this.valuee){
+            this.todo.unshift({text:this.valuee,done:false})
+            this.valuee=""
+        }else{
+          
+        }
+
+        },
+        done(index){
+          if(this.todo[index].done==true){
+            this.todo[index].done=false
+          }else{
+            this.todo[index].done=true
+          }
+        },
+      }
+    
+  }).mount('#app')
